3a7c45d013900eea9107fa00310194aa117e2da4
Yongjun Lee <stareta1202@gmail.com>
2021-10-01 17:17:48 +0900

    Add AddThen

---
+++ AddThen/Classes/AddThen.swift
@@ -0,0 +1,51 @@
+//
+//  AddThen.swift
+//  Pods
+//
+//  Created by Yongjun Lee on 2021/10/01.
+//
+
+import UIKit
+
+extension UIView {
+  @discardableResult
+  func add<T: UIView>(_ subview: T, then closure: ((T) -> Void)? = nil) -> T {
+    addSubview(subview)
+    closure?(subview)
+    return subview
+  }
+  
+  @discardableResult
+  func adds<T: UIView>(_ subviews: [T], then closure: (([T]) -> Void)? = nil) -> [T] {
+    subviews.forEach { addSubview($0) }
+    closure?(subviews)
+    return subviews
+  }
+}
+
+extension UIStackView {
+  @available(iOS 11.0, *)
+  @discardableResult
+  func addArranged<T: UIView>(_ subview: T, spacing: CGFloat? = nil, then closure: ((T) -> Void)? = nil) -> T {
+    addArrangedSubview(subview)
+    if let spacing = spacing {
+      setCustomSpacing(spacing, after: subview)
+    }
+    closure?(subview)
+    return subview
+  }
+  
+  @available(iOS 11.0, *)
+  @discardableResult
+  func addArranged<T: UIView>(_ subviews: [T], spacing: CGFloat? = nil, then closure: (([T]) ->Void)? = nil) -> [T] {
+    for subview in subviews {
+      addArrangedSubview(subview)
+      if let spacing = spacing {
+        setCustomSpacing(spacing, after: subview)
+      }
+    }
+    closure?(subviews)
+    return subviews
+  }
+}
+

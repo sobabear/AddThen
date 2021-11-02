16a643c4dd0f33a53c8769aa11ea2c0abf413e08
stareta1202 <stareta1202@gmail.com>
2021-11-02 23:16:16 +0900

    Support Swift Package Manager

---
+++ Sources/AddThen/AddThen.swift
@@ -0,0 +1,6 @@
+public struct AddThen {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/AddThenTests/AddThenTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import AddThen
+
+final class AddThenTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(AddThen().text, "Hello, World!")
+    }
+}
